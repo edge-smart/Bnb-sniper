306d7c42dc977a9fbfc7a447c368812a6a12e943
edge-smart <devkevin.k.r@gmail.com>
2025-06-21 12:07:41 +0500

    first commit

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb://sniperbot:rbsolutions1122@cluster0-shard-00-00.rqgrj.mongodb.net:27017,cluster0-shard-00-01.rqgrj.mongodb.net:27017,cluster0-shard-00-02.rqgrj.mongodb.net:27017/?replicaSet=atlas-ryvqdx-shard-0&ssl=true&authSource=admin",
+      "mongodb://bnbsniperbot:sniperbot1122@ac-mecsw2e-shard-00-00.ruw984h.mongodb.net:27017,ac-mecsw2e-shard-00-01.ruw984h.mongodb.net:27017,ac-mecsw2e-shard-00-02.ruw984h.mongodb.net:27017/?replicaSet=atlas-ljn4ia-shard-0&ssl=true&authSource=admin",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
